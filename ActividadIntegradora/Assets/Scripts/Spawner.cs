caa2f7a3293940ce5493a2afb278b57a3b6b5030
Esteban de la Maza <A01720548@itesm.mx>
2021-08-30 17:03:55 -0500

    changed spawner

---
+++ ActividadIntegradora/Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject box;
+    public GameObject robot;
+    public int quantityBoxes, quantityRobots, rangeX, rangeZ;
+
+
+    void Start()
+    {
+        for (int i = 0; i < quantityBoxes; i++) {
+            float posX = Random.Range(-rangeX, rangeX);
+            float posZ = Random.Range(-rangeZ, rangeZ);
+            Instantiate(box, new Vector3(posX,0.113f,posZ), Quaternion.identity); // Orient object in +X, +Z, +Y
+        }
+
+        for (int i = 0; i < quantityRobots; i++) {
+            float posX = Random.Range(-rangeX, rangeX);
+            float posZ = Random.Range(-rangeZ, rangeZ);
+            Instantiate(robot, new Vector3(posX,0.019f,posZ), Quaternion.identity); // Orient object in +X, +Z, +Y
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
