4dff2103af8c5a7462f8c01ff96ad5c570d3e2d9
Esteban de la Maza <A01720548@itesm.mx>
2021-08-31 16:38:37 -0500

    added random speed

--- ActividadIntegradora/Assets/Scripts/RobotMovement.cs
+++ ActividadIntegradora/Assets/Scripts/RobotMovement.cs
@@ -15,13 +15,13 @@ public class RobotMovement : MonoBehaviour
     
  // Use this for initialization
     void Start () {
-    
+        speed = Random.Range(1f,6f);
+
     }
 
  // Update is called once per frame
     void Update () {
         float dist = Vector3.Distance(gameObject.transform.position, waypoints[num].transform.position);
-    
         if (go)
         {
         if (dist >minDist)
